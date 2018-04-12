92c2c2f5fcc342039a26c0b1b34a33ce6c41495c
Im Khem <im.khem@gmail.com>
2018-04-12 12:10:13 -0700

    Added linting

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "react/prop-types": 0,
+        "react/no-did-mount-set-state": 0,
+    }
+};
